902aec8583a17d8c0a54ca0fdfb28654308d68df
satoshiota <satoshi.ota928@gmail.com>
2019-07-16 16:42:39 +0900

    clean up & create option use tether or not

--- pyramid_gazebo/src/pyramid_hovering_example.cpp
+++ pyramid_gazebo/src/pyramid_hovering_example.cpp
@@ -64,7 +64,7 @@ int main(int argc, char** argv) {
   trajectory_msg.header.stamp = ros::Time::now();
 
   // Default desired position and yaw.
-  Eigen::Vector3d desired_position(0.0, 0.0, 10.0);
+  Eigen::Vector3d desired_position(0.0, 0.0, 5.0);
   double desired_yaw = 0.0;
 
   // Overwrite defaults if set as node parameters.
