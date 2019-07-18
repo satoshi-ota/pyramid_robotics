092770285e4db3a81972fa6f15690bdc6e2ca234
satoshi-ota <satoshi.ota928@gmail.com>
2019-07-18 18:29:08 +0900

    clean up cmake.txt & package.xml

--- tether_description/creat_tether.py
+++ tether_description/creat_tether.py
@@ -3,20 +3,17 @@
 
 from jinja2 import Environment, FileSystemLoader
 import sys
-#CSVモジュール
+
 import csv
 import subprocess
 
 args = sys.argv
 
-#テンプレートファイルを指定
 env = Environment(loader=FileSystemLoader('./urdf/', encoding='utf8'))
 tpl = env.get_template('tether_fragment.xacro')
 
-#テンプレートへの挿入
 urdf = tpl.render()
 
-#ファイルへの書き込み
 tmpfile = open("./urdf/tether.xacro", 'w') #書き込みモードで開く
 tmpfile.write(urdf.encode('utf-8'))
 tmpfile.close()
