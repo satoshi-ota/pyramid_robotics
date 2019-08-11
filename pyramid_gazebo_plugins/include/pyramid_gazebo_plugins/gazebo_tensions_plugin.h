640cb88917bd73a1d677171908c895f81e101907
satoshi-ota <satoshi.ota928@gmail.com>
2019-08-11 18:48:23 +0900

    Added TensionsPlugin::Load() &TensionsPlugin::Update()

--- pyramid_gazebo_plugins/include/pyramid_gazebo_plugins/gazebo_tensions_plugin.h
+++ pyramid_gazebo_plugins/include/pyramid_gazebo_plugins/gazebo_tensions_plugin.h
@@ -14,30 +14,54 @@ class TensionsPlugin : public ModelPlugin
 {
 public:
     TensionsPlugin();
-    ~TensionsPlugin();
+    ~TensionsPlugin()
+    {
+        rosnode_.shutdown();
+    }
 
-    virtual void Load();
+    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
     virtual void Update();
 
 private: //member function
 
     //tensions distribution
-    void TensionsCommandCB(const pyramid_msgs::TensionsConsrPtr &msg);
+    void TensionsCommandCB(const pyramid_msgs::TensionsConsrPtr &_msg)
+    {
+        tensions_command_ = *_msgs;
+        command_received_ = true;
+    }
 
 private: //data member
-    //subscriber to tensions
-    ros::Subscriber command_subscriber_;
-    sensor_msgs::JointState joint_command_;
+    //general
+    ros::NodeHandle rosnode_;
+    ros::CallbackQueue callback_queue_;
+    event::ConnectionPtr update_event_;
+    double update_T_;
 
-    //publisher to joint_state
+    //model
+    physics::ModelPtr model_;
+
+    //joints
+    std::vector<physics::JointPtr> joints_;
+    double f_max;
+
+    //subscriber to tensions
+    bool use_tether_;
+    ros::Subscriber command_subscriber_;
+    pyramid_msgs::Tensions tensions_command_;
+    bool command_received_;
+
+    //publisher to joint_states
     ros::Publisher joint_state_publisher_;
     sensor_msgs::JointState joint_states_;
 
-    //publisher to end-effector position
-    ros::Publisher ee_publisher_;
-    
-};
+    //publisher to end-effector state
+    ros::Publisher ee_state_publisher_;
+    gazebo_msgs::LinkState ee_state_;
+    physics::LinkPtr ee_link_;
 
+};
+GZ_REGISTER_MODEL_PLUGIN(TensionsPlugin)
 } //namespace gazebo
 
 #endif //PYRAMID_GAZEBO_PLUGINS_GAZEBO_TENSIONS_PLUGIN_H
