bb7e85ff4331be26bbd94d687c0a27859df361fc
satoshi-ota <satoshi.ota928@gmail.com>
2019-08-21 23:45:44 +0900

    Added namespace

--- pyramid_description/tether_description/script/creat_tether.py
+++ pyramid_description/tether_description/script/creat_tether.py
@@ -9,11 +9,11 @@ import subprocess
 
 args = sys.argv
 
-env = Environment(loader=FileSystemLoader('./urdf/', encoding='utf8'))
+env = Environment(loader=FileSystemLoader('../urdf/jinja/', encoding='utf8'))
 tpl = env.get_template('massless_rigid_tether.xacro')
 
 urdf = tpl.render()
 
-tmpfile = open("./urdf/tether.xacro", 'w') #書き込みモードで開く
+tmpfile = open("../urdf/tether.xacro", 'w') #書き込みモードで開く
 tmpfile.write(urdf.encode('utf-8'))
 tmpfile.close()
